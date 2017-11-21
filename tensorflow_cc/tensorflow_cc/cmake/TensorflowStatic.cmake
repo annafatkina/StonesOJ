04c39fcda19796b552005bd6b2ef914fd54334c0
HitriyDmitriy <st022657@student.spbu.ru>
2017-11-21 13:16:06 +0300

    added submodule

---
+++ tensorflow_cc/tensorflow_cc/cmake/TensorflowStatic.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+  tensorflow_static
+  DEPENDS tensorflow_base
+  TMP_DIR "/tmp"
+  STAMP_DIR "tensorflow-stamp"
+  SOURCE_DIR "tensorflow"
+  BUILD_IN_SOURCE 1
+  DOWNLOAD_COMMAND ""
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND tensorflow/contrib/makefile/build_all_linux.sh
+            COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/cmake/copy_links.sh" .
+            COMMAND ./copy_links.sh .
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+)
---
+++ tensorflow_cc/tensorflow_cc/cmake/build_tensorflow.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+# configure environmental variables
+export CC_OPT_FLAGS="-march=native"
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_NEED_OPENCL=0
+export TF_NEED_JEMALLOC=1
+export TF_NEED_VERBS=0
+export TF_NEED_MKL=1
+export TF_DOWNLOAD_MKL=1
+export TF_NEED_MPI=0
+export TF_ENABLE_XLA=1
+export TF_CUDA_CLANG=0
+export PYTHON_BIN_PATH="$(which python3)"
+export PYTHON_LIB_PATH="$($PYTHON_BIN_PATH -c 'import site; print(site.getsitepackages()[0])')"
+
+# configure cuda environmental variables
+if [ -e /usr/local/cuda ]; then
+    echo "CUDA support enabled"
+    cuda_config_opts="--config=cuda"
+    export TF_NEED_CUDA=1
+    export TF_CUDA_COMPUTE_CAPABILITIES="3.0,3.5,5.2,6.1,6.2"
+    export CUDA_TOOLKIT_PATH=/usr/local/cuda
+    export CUDNN_INSTALL_PATH=/usr/local/cuda
+    export TF_CUDA_VERSION="$($CUDA_TOOLKIT_PATH/bin/nvcc --version | sed -n 's/^.*release \(.*\),.*/\1/p')"
+    export TF_CUDNN_VERSION="$(sed -n 's/^#define CUDNN_MAJOR\s*\(.*\).*/\1/p' $CUDNN_INSTALL_PATH/include/cudnn.h)"
+    # use gcc-5 for now, clang in the future
+    export GCC_HOST_COMPILER_PATH=/usr/bin/gcc
+    export CLANG_CUDA_COMPILER_PATH=/usr/bin/clang
+    export TF_CUDA_CLANG=0
+else
+    echo "CUDA support disabled"
+    cuda_config_opts=""
+    export TF_NEED_CUDA=0
+fi
+
+# configure and build
+./configure
+bazel build -j 4 --config=monolithic -c opt $cuda_config_opts --copt=${CC_OPT_FLAGS} tensorflow:libtensorflow_cc.so
+bazel shutdown
